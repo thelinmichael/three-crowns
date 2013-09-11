089fcb80c667b23890aca561204645994c93e4b0
Michael Thelin <thelinmichael@spotify.com>
2013-09-11 22:41:15 +0200

    Can join and leave game, and backend creates and removes players

--- game-server/app.js
+++ game-server/app.js
@@ -22,22 +22,54 @@ var gameSchema = mongoose.Schema({
     start: Date,
     end : Date
 });
+var Game = mongoose.model('Game', gameSchema);
+
+var playerSchema = mongoose.Schema({
+    name: String
+});
+var Player = mongoose.model('Player', playerSchema);
 
 /* Controllers */
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
   socket.emit('connection-attempt', { status: 'success' });
 
-  socket.on('game-status', function (data) {
-    console.log("Incoming game-status", data);
-    switch (data.action) {
-      case "start":
-        console.log("Starting game...");
-        io.sockets.emit("game-status", { action : "start", status : "success" });
-        break;
-      case "end":
-        console.log("Ending game...");
-        io.sockets.emit("game-status", { action : "end", status : "success" });
-        break;
-    }
+  socket.on('join', function(user) {
+    var player = new Player({ name : user.name });
+    player.save(function(err) {
+      if (err) {
+        console.log(err);
+         socket.emit("join", { status : "failed"});
+      } else {
+        console.log("Created player " + user.name);
+        socket.emit("join", { status : "success"});
+        Player.count({}, function(err, count) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Number of players: " + count);
+          }
+        });
+      }
+    });
   });
+
+  socket.on('leave', function(user) {
+    Player.remove({ name : user.name }, function(err) {
+      if (err) {
+        console.log(err)
+        socket.emit("leave", { status : "failed"});
+      } else {
+        console.log("Removed player " + user.name);
+        socket.emit("leave", { status : "success"});
+        Player.count({}, function(err, count) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Number of players: " + count);
+          }
+        })
+      }
+    });
+  });
+
 });
