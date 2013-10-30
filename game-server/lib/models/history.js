fecb5b9fd08ff74b5638f9bf9b53ea62cf6bf155
Michael Thelin <thelinmichael@gmail.com>
2013-10-30 23:19:08 +0100

    Added some documentation.

--- game-server/lib/models/history.js
+++ game-server/lib/models/history.js
@@ -1,3 +1,5 @@
+/* TODO: This module will save the game's history. When a tile is placed, 
+where it was placed, what type of tile it was, and who placed it. */
 var mongoose = require("mongoose");
 var schema = mongoose.Schema({
   moves : ['Move']
--- game-server/lib/models/move.js
+++ game-server/lib/models/move.js
@@ -1,3 +1,4 @@
+/* This module will describe a move that can be used to save and repeat a game's history. */
 var mongoose = require("mongoose");
 var schema = mongoose.Schema({
   player : ['Player']
