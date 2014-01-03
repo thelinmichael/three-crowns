ed7ea3dc8e1bf7b6d5e1d8905c3bd64a2317f309
Michael Thelin <thelinmichael@gmail.com>
2014-01-03 22:21:23 +0100

    Minor refactoring to directions

--- game-server/libs/directions.js
+++ game-server/libs/directions.js
@@ -1,40 +1,51 @@
 /**
- * This class describes directions, and helps make the code more readable, and keeps track of which
- * positions transalte to which direction.
+ * This class describes directions that a tile can face.
+ * North is the same as up.
  */
+
 var Directions = {
   NORTH : 0,
   EAST  : 1,
   SOUTH : 2,
   WEST  : 3,
 
-  /* Directions on the opposite side of {direction} */
+  /**
+   * @param {direction} Number A direction
+   * @returns {Number} Returns a number corresponding to the opposite side of {direction}
+   */
   oppositeOf : function(direction) {
     return (direction + 2) % 4;
   },
 
-  rotateCounterClockwise : function(direction, rotation) {
-    if (direction instanceof Array) {
-      var rotated = direction.map(function(dir) {
-        return _rotateCounterClockwise(dir, rotation);
-      });
-      return rotated;
-    } else {
-      return _rotateCounterClockwise(direction, rotation);
-    }
+  /**
+   * @param {Number|Array} directions The direction or directions that should be rotated counter clockwise
+   * @returns {Number|Array} The {directions} with rotated values
+   * Example: rotateCounterClockwise(Directions.NORTH, Rotations.ONCE) -> Directions.WEST
+   */
+  rotateCounterClockwise : function(directions, rotation) {
+    return _rotate(direction, rotation, _rotateCounterClockwise);
   },
 
-  rotateClockwise : function(direction, rotation) {
-    if (direction instanceof Array) {
-      var rotated = direction.map(function(dir) {
-        return _rotateClockwise(dir, rotation);
-      });
-      return rotated;
-    } else {
-      return _rotateClockwise(direction, rotation);
-    }
+  /**
+   * @param {Number|Array} directions The direction or directions that should be rotated clockwise
+   * @returns {Number|Array} The {directions} with rotated values
+   * Example: rotateClockwise(Directions.NORTH, Rotations.ONCE) -> Directions.EAST
+   */
+  rotateClockwise : function(directions, rotation) {
+    return _rotate(direction, rotation, _rotateClockwise);
   }
 
+};
+
+var _rotate = function(direction, rotation, rotationFunction) {
+  if (direction instanceof Array) {
+    var rotated = direction.map(function(dir) {
+      return rotationFunction(dir, rotation);
+    });
+    return rotated;
+  } else {
+    return rotationFunction(direction, rotation);
+  }
 };
 
 var _rotateClockwise = function(direction, rotation) {
