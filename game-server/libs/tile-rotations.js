357edb32488d249828b7a0e694c536bbe793ec32
Michael Thelin <thelinmichael@gmail.com>
2014-01-03 22:01:26 +0100

    Minor refactoring of tile rotations

--- game-server/libs/tile-rotations.js
+++ game-server/libs/tile-rotations.js
@@ -1,3 +1,8 @@
+/**
+ * This module's purpose is to make it easier to read code that use tile rotation, and to
+ * emphasises that the only options that make sense are 1-3 rotations.
+ */
+
 var Rotations = {
   NONE   : 0,
   ONCE   : 1,
